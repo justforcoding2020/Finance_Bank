f33c73e98313a8461211ef94b5b01af1ccfcb015
Jesse <justfortest@gmail.com>
2022-09-10 21:02:04 +0800

    Senven commit

--- src/main/java/com/atuigu/GitTest.java
+++ src/main/java/com/atuigu/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello jue");
         System.out.println("hello www");
         System.out.println("hello www");
+        System.out.println("hello push");
+        System.out.println("hello push");
     }
 }
